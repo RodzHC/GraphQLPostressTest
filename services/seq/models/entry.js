2a43e14933ffccbc9d3610b69386d901a455d7f3
Rodrigo Marzullo <rodrigo.marzullo@gmail.com>
2019-02-25 09:36:25 -0300

    Adicionando Apollo-server

--- services/seq/models/entry.js
+++ services/seq/models/entry.js
@@ -1,29 +1,31 @@
 const sequelizeConnection = require("./build-db")();
 const bodyParser = require("body-parser");
-const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { ApolloServer, gql } = require("apollo-server-express");
 const logger = require("../helpers/logger");
 var { NODE_ENV } = process.env;
-
 if (!NODE_ENV) {
   NODE_ENV = "development";
 }
 const userSchema = require("./User/schema")(sequelizeConnection);
+
 module.exports = function(app) {
-  app.use("/graphql", bodyParser.json(), (req, res, next) =>
-    graphqlExpress({
-      userSchema,
-      context: {
-        user: req.user
-      }
-    })(req, res, next)
-  );
-  if (NODE_ENV === "development") {
-    app.get(
-      "/graphiql",
-      graphiqlExpress({
-        endpointURL: "/graphql"
-      })
-    );
-  }
-  logger.info(`Running a GraphQL API server at /graphql`);
+  const server = new ApolloServer({typeDefs: ,
+  resolvers:});
+  //   app.use("/graphql", bodyParser.json(), (req, res, next) =>
+  //     graphqlExpress({
+  //       userSchema,
+  //       context: {
+  //         user: req.user
+  //       }
+  //     })(req, res, next)
+  //   );
+  //   if (NODE_ENV === "development") {
+  //     app.get(
+  //       "/graphiql",
+  //       graphiqlExpress({
+  //         endpointURL: "/graphql"
+  //       })
+  //     );
+  //   }
+  //   logger.info(`Running a GraphQL API server at /graphql`);
 };
